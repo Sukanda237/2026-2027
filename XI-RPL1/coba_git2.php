a9851ca16220241d4fc7a4971f33af4569a695bf
sukanda237 <sukandawiguna@mail.unpas.ac.id>
2026-08-18 08:55:02 +0700

    ubah kode

--- XI-RPL1/coba_git2.php
+++ XI-RPL1/coba_git2.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,21 +10,4 @@
 <body>
 
 </body>
-<!-- ubah code  -->
-
-=======
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
->>>>>>> b373e2636f6e0532063ec499638d4e95500ca3eb
-</html>
+<!-- ubah code  --></html>
